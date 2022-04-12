2463f34ad78cf16118950aa2c60398c5842998f0
h2magic <h2magic@163.com>
2022-04-12 22:57:38 +0800

    building model

--- math_mini_guide/models.py
+++ math_mini_guide/models.py
@@ -1,4 +1,27 @@
-from tortoise.models import Model
-from tortoise import fields
+from tortoise import fields, Model
 
-from utils.reference import AbstractBaseModel
+from utils.reference import AbstractBaseModel, random_string, b64encode
+
+
+class Area(AbstractBaseModel):
+    pass
+
+
+class Catalogue(AbstractBaseModel):
+    area = fields.ForeignKeyField('models.Area', description="领域", related_name='catalogue')
+
+
+class Article(AbstractBaseModel):
+    name = fields.CharField(description="文章名", max_length=50)
+    catalogue = fields.ForeignKeyField('models.Category', description="类目", related_name='article')
+    area = fields.ForeignKeyField('models.Area', description="领域", related_name='article')
+    body = fields.TextField(verbose_name="内容")
+
+
+class WechatUser(Model):
+    id = fields.IntField(pk=True)
+    open_id = fields.CharField(description="OpenID", max_length=50, unique=True)
+    signature = fields.CharField(description="签名", max_length=50, default=random_string())
+
+    def token(self):
+        return f"{b64encode(self.open_id)}.{b64encode(self.signature)}"
--- utils/reference.py
+++ utils/reference.py
@@ -1,8 +1,26 @@
+import random
+import string
+import base64
+
 from tortoise import fields, models
 
 
 class AbstractBaseModel(models.Model):
     id = fields.IntField(pk=True)
+    no = fields.IntField(description='序号', default=0)
+    name = fields.CharField(max_length=30, description="名字")
 
     class Meta:
         abstract = True
+
+
+def random_string(length=50):
+    return ''.join(random.sample(string.ascii_letters + string.digits, length))
+
+
+def b64encode(message: str):
+    return base64.b64encode(message.encode()).decode()
+
+
+def b64decode(message: str):
+    return base64.b64decode(message).decode()
