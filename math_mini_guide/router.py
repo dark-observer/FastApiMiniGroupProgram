57958903dbc843bbcdf6d83b21aa413c1c637b90
h2magic <h2magic@163.com>
2022-04-13 07:55:03 +0800

    refactory

--- math_mini_guide/router.py
+++ math_mini_guide/router.py
@@ -27,8 +27,8 @@ async def post_area(pk: int, area: PydanticArea):
 @router.get('/full-catalogue')
 async def full_catalogue(request: Request):
     q = Q()
-    if 'area' in request.query_params:
-        q &= Q(area_id=request.query_params.get('area'))
+    if area := request.query_params.get('area'):
+        q &= Q(area_id=area)
     return await Catalogue.filter(q)
 
 
@@ -46,10 +46,10 @@ async def post_catalogue(pk: int, catalogue: PydanticCatalogue):
 @router.get('/full-article')
 async def full_article(request: Request):
     q = Q()
-    if 'area' in request.query_params:
-        q &= Q(area_id=request.query_params.get('area'))
-    if 'catalogue' in request.query_params:
-        q &= Q(catalogue_id=request.query_params.get('catalogue'))
+    if area := request.query_params.get('area'):
+        q &= Q(area_id=area)
+    if catalogue := request.query_params.get('catalogue'):
+        q &= Q(catalogue_id=catalogue)
     return await Article.filter(q)
 
 
