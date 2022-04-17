a96560f4d592c8e8fe5e6486ff8cfefb2439767f
h2magic <h2magic@163.com>
2022-04-17 20:10:12 +0800

    增加token验证和白名单

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
 from fastapi.middleware.cors import CORSMiddleware
 from tortoise.contrib.fastapi import register_tortoise
 
 from math_mini_guide.router import router as math_mini_guide_router
 from math_mini_guide.admin_router import router as admin_router
+from math_mini_guide.models import WechatUser
+from utils.reference import b64encode
 
 from utils.settings import DATABASE, DATABASE_MODELS, CORS_ALLOW_ORIGINS
+
+WHITELIST = [
+    '/admin'
+]
 
 app = FastAPI()
 
@@ -20,6 +26,22 @@ app.add_middleware(
 
 app.include_router(math_mini_guide_router)
 app.include_router(admin_router)
+
+
+@app.middleware('http')
+async def add_process_time_header(request: Request, call_next):
+    for white_list in WHITELIST:
+        if request.url.path.startswith(white_list):
+            return await call_next(request)
+
+    if token := request.headers.get('Token'):
+        payload, signature = token.split('.')
+        count = await WechatUser.filter(open_id=b64encode(payload), signature=b64encode(signature)).count()
+        if count > 0:
+            return await call_next(request)
+
+    return Response('身份未验证'.encode('GB18030'))
+
 
 # 所有应用的model注册到数据库
 register_tortoise(
--- math_mini_guide/admin_router.py
+++ math_mini_guide/admin_router.py
@@ -1,3 +1,9 @@
 from fastapi import APIRouter
+from tortoise.models import Q
+from starlette.requests import Request
+from fastapi.responses import ORJSONResponse
+
+from math_mini_guide.models import Area, Catalogue, Article, WechatUser
+from math_mini_guide.pydantics import PydanticArea, PydanticCatalogue, PydanticArticle, PydanticWechatUser
 
 router = APIRouter(prefix='/admin')
